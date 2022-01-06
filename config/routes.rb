0fbd423a5aa2da439024eb6cefa36885240f9ab5
Bishoy <bishoy.samwuel@gmail.com>
2022-01-06 15:10:43 +0200

    Add root rout and randomMessage route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
   namespace :api do
     namespace :v1 do
-      resources :messages, only: %i[index create destroy]
+      resources :messages, only: %i[index create show update destroy]
+      get 'randomMessage', to: 'messages#random_message'
     end
   end
 end
