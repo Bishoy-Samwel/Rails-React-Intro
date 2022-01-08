069208bd752854131ca35b122cc804f22a57e42b
Bishoy <bishoy.samwuel@gmail.com>
2022-01-08 13:23:35 +0200

    Import React, ReactDOM

--- app/javascript/components/clock.jsx
+++ app/javascript/components/clock.jsx
@@ -1,3 +1,6 @@
+import React from "react"
+import ReactDOM from 'react-dom'
+
 function Clock(props) {
   return (
     <div>
@@ -13,5 +16,6 @@ function tick() {
     document.getElementById('root')
   );
 }
+
 
 setInterval(tick, 1000);
