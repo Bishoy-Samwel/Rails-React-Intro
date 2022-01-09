f0242be1684760659254e0d2117e835be452d34c
Bishoy <bishoy.samwuel@gmail.com>
2022-01-09 16:55:02 +0200

    Add list of links, each one call a specific component

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,22 +1,53 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+
 import Message from './message.jsx'
 
-const App = () => {
-    return (
-      <Message msg="hi" />
-      // <Router>
-      //   <Switch>
-      //     <Route exact path="/" render={()=> <Message msg="hi" />}/>
-      //     <Route path="/message" render={() => <Message msg="hi" />} />
-      //   </Switch>
-      // </Router>
-    );
+
+
+
+
+
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              <Link to="/randomMessage">Random Message</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/randomMessage" element={<Message msg="hi"/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
-
+function Home() {
+  return <h2>Home</h2>;
+}
 
 ReactDOM.render(
   <App />,
