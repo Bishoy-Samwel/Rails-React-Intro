fdc210eb0bd2cc76fe5898485f6b4099de9c5bd9
Bishoy <bishoy.samwuel@gmail.com>
2022-01-09 22:21:46 +0200

    Add action creator => GetRandomMsg dispatch an action for the button Select the message value from the store

--- app/javascript/components/message.jsx
+++ app/javascript/components/message.jsx
@@ -1,11 +1,24 @@
 import React from "react"
-import ReactDOM from 'react-dom'
+import { useSelector, useDispatch } from "react-redux";
 
+const GET_RANDOM_MSG = 'GET_RANDOM_MSG';
+
+GetRandomMsg = () => {
+  console.log('GetRandomMsg() Action')
+  return {
+    type: GET_RANDOM_MSG
+  }
+}
 
 export default function Message(props) {
+  const { msg } = props;
+  const dispatch = useDispatch();
+  const message = useSelector((state) => state.msg)
   return (
     <div>
-      <h2>The Message is {props.msg}.</h2>
+      <h2>The Message from props is {msg}.</h2>
+      <button className="getMessageBtn" onClick={() => dispatch(GetRandomMsg())}> Get Random Message</button>
+      <h2>The Message from store is {message.title}.</h2>
     </div>
   );
 }
