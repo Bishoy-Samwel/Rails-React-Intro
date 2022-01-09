c2841001c2c09198f31d392795a7898a1e18dddd
Bishoy <bishoy.samwuel@gmail.com>
2022-01-09 16:59:38 +0200

    send requests not for the API to App component (via ComponentsController#index)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root 'components#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  get '*page', to: 'components#index', constraints: -> (req) do
+    !req.xhr? && req.format.html?
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api do
     namespace :v1 do
       resources :messages, only: %i[index]
