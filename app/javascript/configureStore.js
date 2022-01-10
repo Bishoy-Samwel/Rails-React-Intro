5458069de0af47415e09bdaa042749c68db11ca0
Bishoy <bishoy.samwuel@gmail.com>
2022-01-10 10:56:27 +0200

    Add a case GET_RANDOM_MSG_SUCCSESS to return msg Apply Middleware thunk

--- app/javascript/configureStore.js
+++ app/javascript/configureStore.js
@@ -1,4 +1,6 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
 const initialState = {
   msg: { title: "Hello", body: "English Message" }
@@ -7,12 +9,17 @@ const initialState = {
 rootReducer = (state = initialState, action) => {
   console.log(action.type, "request");
   switch (action.type) {
+    case "GET_RANDOM_MSG_SUCCSESS":
+      return { msg: action.json };
     default:
       return state
   }
 }
 
 export default configureStore = () => {
-  const store = createStore(rootReducer, initialState);
+  const store = createStore(rootReducer, initialState,
+    composeWithDevTools(
+      applyMiddleware(thunk)
+    ));
   return store;
 }
